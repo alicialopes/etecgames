7088137ab4368e74cc3b147a1343b64dad49abbf
Alícia Lopes <alicia.lopes04@gmail.com>
2021-08-18 22:12:01 -0300

    adicionando a view/controller do usuario

---
+++ app/Controllers/Usuario.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controllers;
+
+Class Usuario extends BaseController{
+
+    public function index(){
+        echo view('header');
+		echo view('cadUsuario');
+		echo view('footer');
+    }
+
+    public function inserirUsuario(){
+        
+    }
+    
+}
